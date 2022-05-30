2c8cdf4b70c13a4c32f7d22a54a12ab136981db8
Alfred <alfredleo@gmail.com>
2022-05-31 01:18:46 +0500

    Add group tests as a function names.

--- tests/IPTest.php
+++ tests/IPTest.php
@@ -13,6 +13,34 @@ class IPTest extends TestCase
         $locator = new Locator();
         $location = $locator->locate('8.8.8.8');
         self::assertNotNull($location);
+        self::assertEquals('success', $location['status']);
+    }
+
+    public function testNotFound(): void
+    {
+        $locator = new Locator();
+        $location = $locator->locate('127.0.0.1');
+        self::assertNull($location);
+    }
+
+    public function testInvalidIP(): void
+    {
+        $locator = new Locator();
+        $location = $locator->locate('invalid');
+        self::assertNull($location);
+    }
+
+    public function testEmptyIp(): void
+    {
+        $locator = new Locator();
+        $location = $locator->locate('');
+        self::assertNull($location);
+    }
+
+    public function testFieldsEqualTo(): void
+    {
+        $locator = new Locator();
+        $location = $locator->locate('8.8.8.8');
         self::assertEquals('United States', $location['country']);
         self::assertEquals('US', $location['countryCode']);
         self::assertEquals('VA', $location['region']);
@@ -25,6 +53,5 @@ class IPTest extends TestCase
         self::assertEquals('Google LLC', $location['isp']);
         self::assertEquals('Google Public DNS', $location['org']);
         self::assertEquals('AS15169 Google LLC', $location['as']);
-        self::assertEquals('8.8.8.8', $location['query']);
     }
 }
