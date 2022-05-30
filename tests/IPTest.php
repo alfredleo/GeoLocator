f0aec9710d6ddc6b1e4a9c346ca3c5b6698ac07e
Alfred <alfredleo@gmail.com>
2022-05-31 01:04:54 +0500

    Add test for each field of ip-api.com. Fix color output when using with composer

--- tests/IPTest.php
+++ tests/IPTest.php
@@ -12,6 +12,19 @@ class IPTest extends TestCase
     {
         $locator = new Locator();
         $location = $locator->locate('8.8.8.8');
-        $this->assertTrue($location['countryCode'] === 'US');
+        self::assertNotNull($location);
+        self::assertEquals('United States', $location['country']);
+        self::assertEquals('US', $location['countryCode']);
+        self::assertEquals('VA', $location['region']);
+        self::assertEquals('Virginia', $location['regionName']);
+        self::assertEquals('Ashburn', $location['city']);
+        self::assertEquals('20149', $location['zip']);
+        self::assertEquals('39.03', $location['lat']);
+        self::assertEquals('-77.5', $location['lon']);
+        self::assertEquals('America/New_York', $location['timezone']);
+        self::assertEquals('Google LLC', $location['isp']);
+        self::assertEquals('Google Public DNS', $location['org']);
+        self::assertEquals('AS15169 Google LLC', $location['as']);
+        self::assertEquals('8.8.8.8', $location['query']);
     }
 }
